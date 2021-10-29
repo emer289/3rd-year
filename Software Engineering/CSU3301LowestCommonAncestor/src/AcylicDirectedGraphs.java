58e78782d809fa91988b306f567881fd928fa9f5
emer289 <murphe65@tcd.ie>
2021-10-29 12:18:48 +0100

    add transpose function
    deleted old code in java. I have decided to change my approach.

--- Software Engineering/CSU3301LowestCommonAncestor/src/AcylicDirectedGraphs.java
+++ Software Engineering/CSU3301LowestCommonAncestor/src/AcylicDirectedGraphs.java
@@ -1,64 +1,80 @@
-// Java program to find single source shortest paths in Directed Acyclic Graphs
-import java.io.*;
+// Java program to find the transpose of a graph
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-class AcylicDirectedGraphs
+class Graph
 {
-	static final int INF=Integer.MAX_VALUE;
-	class AdjListNode
-	{
-		private int v;
-		private int weight;
-		AdjListNode(int _v, int _w) { v = _v; weight = _w; }
-		int getV() { return v; }
-		int getWeight() { return weight; }
-	}
-
-	// Class to represent graph as an adjacency list of
-	// nodes of type AdjListNode
-	class Graph
-	{
-		private int V;
-		private LinkedList<AdjListNode>adj[];
-		Graph(int v)
-		{
-			V=v;
-			adj = new LinkedList[V];
-			for (int i=0; i<v; ++i)
-				adj[i] = new LinkedList<AdjListNode>();
-		}
-		void addEdge(int u, int v, int weight)
-		{
-			AdjListNode node = new AdjListNode(v,weight);
-			adj[u].add(node);// Add v to u's list
-		}
+	// Total number of vertices
+	private static int vertices = 5;
 	
-	}
-
-	// Method to create a new graph instance through an object
-	// of ShortestPath class.
-	Graph newGraph(int number)
-	{
-		return new Graph(number);
-	}
-
-	public static void main(String args[])
-	{
-		// Create a graph given in the above diagram. Here vertex
-		// numbers are 0, 1, 2, 3, 4, 5 with following mappings:
-		// 0=r, 1=s, 2=t, 3=x, 4=y, 5=z
-		AcylicDirectedGraphs t = new AcylicDirectedGraphs();
-		Graph g = t.newGraph(6);
-		g.addEdge(0, 1, 5);
-		g.addEdge(0, 2, 3);
-		g.addEdge(1, 3, 6);
-		g.addEdge(1, 2, 2);
-		g.addEdge(2, 4, 4);
-		g.addEdge(2, 5, 2);
-		g.addEdge(2, 3, 7);
-		g.addEdge(3, 4, -1);
-		g.addEdge(4, 5, -2);
+	// Find transpose of graph represented by adj
+	private static ArrayList<Integer>[] adj = new ArrayList[vertices];
 	
+	// Store the transpose of graph represented by tr
+	private static ArrayList<Integer>[] tr = new ArrayList[vertices];
+
+	// Function to add an edge from source vertex u to
+	// destination vertex v, if choice is false the edge is added
+	// to adj otherwise the edge is added to tr
+	public static void addedge(int u, int v, boolean choice)
+	{
+		if(!choice)
+			adj[u].add(v);
+		else
+			tr[u].add(v);
+	}
+
+	// Function to print the graph representation
+	public static void printGraph()
+	{
+		for(int i = 0; i < vertices; i++)
+		{
+			System.out.print(i + "--> ");
+			for(int j = 0; j < tr[i].size(); j++)
+				System.out.print(tr[i].get(j) + " ");
+			System.out.println();
+		}
+	}
+
+	// Function to print the transpose of
+	// the graph represented as adj and store it in tr
+	public static void getTranspose()
+	{
+
+		// Traverse the graph and for each edge u, v
+		// in graph add the edge v, u in transpose
+		for(int i = 0; i < vertices; i++)
+			for(int j = 0; j < adj[i].size(); j++)
+				addedge(adj[i].get(j), i, true);
+	}
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i = 0; i < vertices; i++)
+		{
+			adj[i] = new ArrayList<Integer>();
+			tr[i] = new ArrayList<Integer>();
+		}
+		addedge(0, 1, false);
+		addedge(0, 2, false);
+		addedge(0, 3, false);
+        addedge(0, 4, false);
+        addedge(1, 3, false);
+        addedge(2, 4, false);
+		addedge(3, 4, false);
+		
+		// Finding transpose of the graph
+		getTranspose();
+		
+		// Printing the graph representation
+		//printGraph();
+        System.out.print("parent Nodes are " );
+        System.out.print(3 + "--> ");
+			for(int j = 0; j < tr[3].size(); j++)
+				System.out.print(tr[3].get(j) + " ");
+			System.out.println();
 	}
 }
-//This code is contributed by Aakash Hasija
+
+// This code is contributed by code_freak
