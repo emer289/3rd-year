63a058c4efcbdd48c867837ff3b66b552473ba53
emer289 <murphe65@tcd.ie>
2021-10-31 12:16:02 +0000

    simplified bfs and graph functions
    code from https://blog.kotlin-academy.com/kotlin-for-interviews-part-5-frequently-used-code-snippets-444ad4d137f5

--- Software Engineering/CSU3301LowestCommonAncestor/src/DAG.kt
+++ Software Engineering/CSU3301LowestCommonAncestor/src/DAG.kt
@@ -1,66 +1,54 @@
 import java.util.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
-var vert: MutableList<Color> = mutableListOf()
-var adj: MutableList<MutableList<Pair<Int, Int>>> = mutableListOf()
-
-val sc = BufferedReader(InputStreamReader(System.`in`))
 
 enum class Color {
     BLACK, WHITE
 }
+data class Node(var color: Color,var value: Int, var inDegree: Int, var outDegree: Int, var childNodes: List<Node>)
 
-data class Node(var color: Color, var distance: Int = Int.MAX_VALUE, var parent: Int = -1);
-
-var nodes: MutableList<Node> = mutableListOf()
-
-fun bfs(u: Int) {
-    val queue = mutableListOf<Int>()
-//    val tmp: MutableList<Node> = Collections.nCopies(10, Node(Color.WHITE))
-    nodes[u].distance = 0
-    queue.add(u)
-
-    while(!queue.isEmpty()) {
-        val current = queue.removeAt(0)
-        if(nodes[current].color == Color.WHITE) {
-            nodes[current].color = Color.BLACK
-            adj[current].forEach {
-                if (nodes[it.first].distance == Int.MAX_VALUE) {
-                    nodes[it.first].distance = nodes[current].distance + it.second
-                    nodes[it.first].parent = it.first
-                    queue.add(it.first)
-                }
-            }
+fun graph(pairs: Array<IntArray>) {
+    val graph: HashMap<Int, MutableList<Int>> = hashMapOf()
+    pairs.forEach { pair ->
+        if (!graph.containsKey(pair[0])) {
+            // If the current node isn't in the adjacency list yet,
+            // add it and create its dependency list starting with
+            // pair[1]
+            graph[pair[0]] = mutableListOf(pair[1])
+        } else {
+            // Otherwise, append pair[1] to its existing dependency
+            // list.
+            val dependencies = graph[pair[0]]
+            dependencies!!.add(pair[1])
+            graph[pair[0]] = dependencies
         }
     }
-
-    val visitedNodes = nodes.filter { it.color == Color.BLACK }
-    println("u: $u and $visitedNodes")
 }
 
-fun main(args: Array<String>) {
-   // var tmp = sc.readLine()
-    var tmp1 = arrayOf("9 7", "0 1", "1 2", "1 3", "2 3", "3 4", "6 7", "6 8")
-    val (v, e) = tmp1[0].split(" ").map { it.toInt() }
+fun bfs(nodes: List<List<Node>>) {
+    val visited = BooleanArray(nodes.size) { false }
+    // Create a queue and add 0 to represent the index of the
+    // first node
+    val queue: MutableList<Int> = mutableListOf(0)
+    while (queue.isNotEmpty()) {
+        println("hello")
+        // Dequeue a node from queue
+        val node = queue.removeAt(0)
+        // Add all of the node's unvisited neighbors to the queue
+        println(node)
+        if (!visited[node]) {
+            nodes[node].forEach {
+                queue.add(it.value)
+            }
+            // Mark the dequeued node as visited
+            visited[node] = true
+            println(node)
+            println(visited[node])
 
-    (0..v-1).forEach { nodes.add(Node(Color.WHITE)) }
-    (0..v-1).forEach { adj.add(mutableListOf()) }
-
-    (0..v-1).forEach { nodes[it] = Node(Color.WHITE) }
-    (0..e-1).forEach {
-       // tmp = sc.readLine()
-        tmp1.filter { b -> b[0].equals(tmp1[0]) }.map { val (tv, tw) = it.split(" ").map { a-> a.toInt() }
-            adj[tv].add(Pair(tw, 1))
-            adj[tw].add(Pair(tv, 1)) }
-
+        }
     }
+}
 
-    bfs(0)
-    nodes = mutableListOf()
-    (0..v-1).forEach { nodes.add(Node(Color.WHITE)) }
-    bfs(6)
-    nodes = mutableListOf()
-    (0..v-1).forEach { nodes.add(Node(Color.WHITE)) }
-    bfs(5)
+
+
+fun main(args: Array<String>) {
+
 }
