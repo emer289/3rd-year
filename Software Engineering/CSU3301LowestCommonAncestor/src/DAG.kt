975fe30ee5a7a3cd2c57b277f23163af771765c2
emer289 <murphe65@tcd.ie>
2021-10-31 12:40:36 +0000

    added lca

--- Software Engineering/CSU3301LowestCommonAncestor/src/DAG.kt
+++ Software Engineering/CSU3301LowestCommonAncestor/src/DAG.kt
@@ -3,39 +3,22 @@ import java.util.*
 enum class Color {
     BLACK, WHITE
 }
-data class Node(var color: Color,var value: Int, var inDegree: Int, var outDegree: Int, var childNodes: List<Node>)
+data class Node(val color: Color,val value: Int, val inDegree: Int, val outDegree: Int, val connectedNodes: List<Node>)
 
-fun graph(pairs: Array<IntArray>) {
-    val graph: HashMap<Int, MutableList<Int>> = hashMapOf()
-    pairs.forEach { pair ->
-        if (!graph.containsKey(pair[0])) {
-            // If the current node isn't in the adjacency list yet,
-            // add it and create its dependency list starting with
-            // pair[1]
-            graph[pair[0]] = mutableListOf(pair[1])
-        } else {
-            // Otherwise, append pair[1] to its existing dependency
-            // list.
-            val dependencies = graph[pair[0]]
-            dependencies!!.add(pair[1])
-            graph[pair[0]] = dependencies
-        }
-    }
-}
+data class Graph(val graph: List<List<Node>>)
 
-fun bfs(nodes: List<List<Node>>) {
-    val visited = BooleanArray(nodes.size) { false }
+fun bfs(nodes: Graph) {
+    val visited = BooleanArray(nodes.graph.size) { false }
     // Create a queue and add 0 to represent the index of the
     // first node
     val queue: MutableList<Int> = mutableListOf(0)
     while (queue.isNotEmpty()) {
-        println("hello")
         // Dequeue a node from queue
         val node = queue.removeAt(0)
         // Add all of the node's unvisited neighbors to the queue
         println(node)
         if (!visited[node]) {
-            nodes[node].forEach {
+            nodes.graph[node].forEach {
                 queue.add(it.value)
             }
             // Mark the dequeued node as visited
@@ -46,9 +29,12 @@ fun bfs(nodes: List<List<Node>>) {
         }
     }
 }
-
+fun lca(graph: Graph, node1: Node, node2: Node): Set<Node> {
+    bfs(graph)
+    val sum = node1.connectedNodes + node2.connectedNodes
+    return sum.toSet()
+}
 
 
 fun main(args: Array<String>) {
-
 }
