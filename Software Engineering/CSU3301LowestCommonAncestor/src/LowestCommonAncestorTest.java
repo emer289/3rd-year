9dcf292b7b7abe6793ac489656f83441c3091df3
emer289 <murphe65@tcd.ie>
2021-10-31 18:24:11 +0000

    Starting over ....again
    my solution doesn't work if the LCA of the two nodes are not direct child notes of the LCA node so I have decided to start over. This is frustrating but I feel that I fully understand the assignment now.

--- Software Engineering/CSU3301LowestCommonAncestor/src/LowestCommonAncestorTest.java
+++ Software Engineering/CSU3301LowestCommonAncestor/src/LowestCommonAncestorTest.java
@@ -1,15 +1,17 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
-public class LowestCommonAncestorTest {
+/*public class LowestCommonAncestorTest {
     @Test
     // checking lowestCommonAncestor is giving the expected result for a tree with 7 nodes
     public void testLowestCommonAncestor7Nodes() {
         
         //Set up
         
-        LowestCommonAncestor tree = new LowestCommonAncestor();
+    /*    LowestCommonAncestor tree = new LowestCommonAncestor();
         tree.root = new Node(1);
         tree.root.left = new Node(2);
         tree.root.right = new Node(3);
@@ -17,15 +19,36 @@ public class LowestCommonAncestorTest {
         tree.root.left.right = new Node(5);
         tree.root.right.left = new Node(6);
         tree.root.right.right = new Node(7);
+       
+        AcylicDirectedGraphs g = new AcylicDirectedGraphs();
+        g.vertices = 7;
+		g.adj = new ArrayList[g.vertices];
+		g.tr = new ArrayList[g.vertices];
+		for(int i = 0; i < g.vertices; i++)
+		{
+			g.adj[i] = new ArrayList<Integer>();
+			g.tr[i] = new ArrayList<Integer>();
+		}
+		
+		g.addedge(g.adj,  g.tr, 0, 1, false);
+		g.addedge(g.adj,  g.tr, 0, 2, false);
+		g.addedge( g.adj,  g.tr, 1, 3, false);
+        g.addedge(g.adj,  g.tr, 1, 4, false);
+        g.addedge( g.adj,  g.tr, 2, 5, false);
+        g.addedge(g.adj,  g.tr, 2, 6, false);
+				
+		System.out.println(g.getParentNodes(g.adj,  g.tr, g.vertices, 3).toString());
+
+		
         
         //assertEquals to get actual value
-        assertEquals( 2, tree.findLCA(4,5));
+       /* assertEquals( 2, tree.findLCA(4,5));
         assertEquals( 1, tree.findLCA(4,6));
         assertEquals( 1, tree.findLCA(3,4));
-        assertEquals( 2, tree.findLCA(2,4));
-    }
+        assertEquals( 2, tree.findLCA(2,4));*/
+//    }
 
-    @Test
+   /* @Test
     // checking lowestCommonAncestor is returns -1 for a tree with 0 nodes
     public void testLowestCommonAncestorNoNodes() {
         
@@ -88,4 +111,4 @@ public class LowestCommonAncestorTest {
         
     }
 
-}
+}*/
